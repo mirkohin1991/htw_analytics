b8814f7a3656c2915c27be8502c7b0cf07f01fb9
D065372 <mirko.hin@live.de>
2018-10-10 09:43:13 +0200

    init

---
+++ test.py
@@ -0,0 +1,43 @@
+import datetime
+import json
+a = 1 + 2
+print(a)
+if a == 3:
+    print(datetime.datetime.now())
+
+
+
+#########
+# Here we have so class coding
+#########
+class newClass:
+    def method(self):
+        return 'Test'
+
+
+a = newClass
+print(a.method(a))
+
+
+########
+# Working with the JSON module
+########
+object = """{ "Name" : "Mirko", "Hobbies": ["Sport", "Lesen"], "Alter": 10 }"""
+encoded_obj = json.dumps(object)
+print("Encoded Object", encoded_obj)
+# Formatting of the JSON. Indent = Spaces
+print("Formatted object", json.dumps(object, indent= 4))
+decoded_obj = json.loads(encoded_obj)
+print("Decoded Object", decoded_obj)
+loaded_obj = json.loads(object)
+print("Loaded Object", loaded_obj)
+
+
+
+
+
+
+
+
+
+
